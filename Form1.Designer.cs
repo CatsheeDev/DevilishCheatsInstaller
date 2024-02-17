e4fa1a60af29f1be8ca4bd7e3594a530c683cf85
Cats <cheesychilli0@gmail.com>
2024-02-17 18:48:51 +0000

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DevilishCheatsInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.title = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Comic Sans MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(-5, 108);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(569, 55);
+            this.title.TabIndex = 0;
+            this.title.Text = "Devilish Cheats v2.0.0-beta";
+            this.title.Click += new System.EventHandler(this.title_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(136, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(172, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Uninstall";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(558, 282);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.title);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(574, 321);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(574, 321);
+            this.Name = "Form1";
+            this.Text = "Devilish Cheats Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevilishCheatsInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            downloadShit("https://github.com/CatsheeDev/devilishcheats-files/raw/main/devilishcheats", @"C:\Program Files (x86)\Steam\steamapps\common\Cuphead\Cuphead_Data\StreamingAssets\devilishcheats", false);
+            downloadShit("https://github.com/CatsheeDev/devilishcheats-files/raw/main/Assembly-CSharp.dll", @"C:\Program Files (x86)\Steam\steamapps\common\Cuphead\Cuphead_Data\Managed\Assembly-CSharp.dll", true);
+        }
+
+        private void downloadShit(string fileUrl, string localPath, bool final)
+        {
+            try
+            {
+                if (File.Exists(localPath))
+                {
+                    string backupPath = localPath + ".bak";
+                    File.Move(localPath, backupPath);
+                }
+
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadFile(fileUrl, localPath);
+                    
+                    if (final)
+                    {
+                        MessageBox.Show("Injected Devilish Cheats! Please launch Cuphead.");
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Error downloading file: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("UKNOWN ERROR!!!!!!!!!!!!!!!!!! " + ex.Message);
+            }
+        }
+
+        private void RestoreBackups()
+        {
+            RestoreBackupIfExists("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cuphead\\Cuphead_Data\\StreamingAssets\\devilishcheats");
+            RestoreBackupIfExists("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cuphead\\Cuphead_Data\\Managed\\Assembly-CSharp.dll");
+        }
+
+        private void RestoreBackupIfExists(string path)
+        {
+            string backupPath = path + ".bak";
+            if (File.Exists(backupPath))
+            {
+                File.Delete(path); 
+                File.Move(backupPath, path); 
+            }
+        }
+
+        private void DeleteFiles(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+            else if (File.Exists(path))
+            {
+                File.Delete(path); 
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                DeleteFiles("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cuphead\\Cuphead_Data\\StreamingAssets\\devilishcheats");
+                DeleteFiles("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cuphead\\Cuphead_Data\\Managed\\Assembly-CSharp.dll");
+
+                RestoreBackups(); 
+                MessageBox.Show("Cheats uninstalled successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during uninstall: " + ex.Message);
+            }
+        }
+    }
+}
